aa48e444625a6e3f09e81967036b516676973f55
igorKopylov <igor.kopylov.08d@inbox.ru>
2022-08-05 11:10:51 +0300

    доделал notFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  text-align: center;
+  margin-top: 200px;        
+`;
 
 const NotFound = () => {
     return (
-        <div>NotFound</div>
+        <Wrapper>
+            <h1 style={{ fontSize: '70px' }}>😕</h1>
+            <h1 style={{ fontSize: '60px' }}>Oops...</h1>
+            <p style={{ fontSize: '40px' }}>Sorry, the page is not accessible.</p>
+        </Wrapper>
     )
 }
 
